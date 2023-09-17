bf1156fe7c5fcbc1fcf110da3b314132e0291398
mwaseem75 <mwaseem75@hotmail.com>
2023-09-17 16:46:19 +0300

    commit

--- python/app.py
+++ python/app.py
@@ -7,14 +7,6 @@ if __name__ == "__main__":
     database_uri = f'iris://{DB_USER}:{DB_PASS}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}'
     app = create_app(database_uri)
     Markdown(app)
-    app.run('0.0.0.0', port="4040", debug=True)
-
-
-def main():
-    # get db info from config file
-    database_uri = f'iris://{DB_USER}:{DB_PASS}@{DB_URL}:{DB_PORT}/{DB_NAMESPACE}'
-    app = create_app(database_uri)
-    Markdown(app)
-    app.run('0.0.0.0', port="4040", debug=True)
+    app.run('0.0.0.0', port="4040", debug=False)
 
 
