4336e018c0be6a3b11584fb25e67b75078c9751d
mwaseem75 <mwaseem75@hotmail.com>
2023-09-21 15:16:47 +0300

    commit

--- demo/python/NER/ner.py
+++ demo/python/NER/ner.py
@@ -1,17 +1,24 @@
 from spacy import displacy
 import spacy
-HTML_WRAPPER = """<div style="NER Detected">{}</div>"""
+import re,os
 #funtion used in PEX production BS
 def getNER(raw_text):    
     result = ''            
     if len(raw_text.strip()) > 0:
         # Load English tokenizer, tagger, parser and NER
-        nlp = spacy.load('en_core_web_sm')
-        docx = nlp(raw_text)       
+        nlp = spacy.load('en_core_web_sm')        
+        docx = nlp(raw_text)                      
+        pattern = re.compile('<.*?>')
         html = displacy.render(docx, style="ent")
-        html = html.replace("\n\n", "\n")
-        result = HTML_WRAPPER.format(html)
-        return result
+        html = html.replace("\n\n", "\n")        
+        result = re.sub(pattern, '', html).strip()
+        without_empty_lines = os.linesep.join(
+           [
+                line for line in result.splitlines()
+                if line
+            ]
+        )        
+        return without_empty_lines
     return result    
      
 
